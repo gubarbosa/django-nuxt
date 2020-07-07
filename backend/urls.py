94562524aaf9156d54643d154db9d75da21e1afa
gubarbosa <gugabarbosa.bsi@gmail.com>
2020-07-07 00:26:25 -0300

    atualizando as rotas no urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from backend.core.router import router
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token"),
+    path("api/", include(router.urls)),
     path("api/refresh_token/", TokenRefreshView.as_view(), name="refresh_token"),
 ]
